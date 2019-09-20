7c3326eecbe544517e757a4dbc9f1da587f9d403
neilshi <627911861@qq.com>
2019-09-20 16:53:49 +0800

    twoSum hashMap solution

--- src/main/java/TwoSum.java
+++ src/main/java/TwoSum.java
@@ -1,3 +1,5 @@
+import java.util.HashMap;
+
 /**
  * 给定一个整数数组 nums 和一个目标值 target，请你在该数组中找出和为目标值的那 两个 整数，并返回他们的数组下标。
  * 你可以假设每种输入只会对应一个答案。但是，你不能重复利用这个数组中同样的元素。
@@ -21,6 +23,24 @@ public class TwoSum {
                 }
             }
         }
-        return new int[2];
+        throw new IllegalArgumentException("No two sum solution");
+    }
+
+    /**
+     * 哈希表
+     * 时间复杂度: O(n)
+     * 空间复杂度: O(n)
+     * */
+    public int[] twoSum2(int[] nums, int target) {
+        HashMap<Integer, Integer> map = new HashMap<Integer, Integer>();
+        for (int i = 0; i < nums.length; i++) {
+            int sub = target - nums[i];
+            if (map.containsKey(sub) && map.get(sub) != i) {
+                return new int[]{map.get(sub), i};
+            }
+            map.put(nums[i], i);
+        }
+
+        throw new IllegalArgumentException("No two sum solution");
     }
 }
