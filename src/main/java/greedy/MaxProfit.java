13eebc60408f8abc89c02244d94ab3c1a17d0dcf
neilshi <627911861@qq.com>
2019-10-11 23:25:27 +0800

    ac MaxProfit

---
+++ src/main/java/greedy/MaxProfit.java
@@ -0,0 +1,37 @@
+package greedy;
+
+/**
+ * 给定一个数组，它的第 i 个元素是一支给定股票第 i 天的价格。
+ * <p>
+ * 设计一个算法来计算你所能获取的最大利润。你可以尽可能地完成更多的交易（多次买卖一支股票）。
+ * <p>
+ * 注意：你不能同时参与多笔交易（你必须在再次购买前出售掉之前的股票）。
+ * <p>
+ * 示例 1:
+ * <p>
+ * 输入: [7,1,5,3,6,4]
+ * 输出: 7
+ * 解释: 在第 2 天（股票价格 = 1）的时候买入，在第 3 天（股票价格 = 5）的时候卖出, 这笔交易所能获得利润 = 5-1 = 4 。
+ *      随后，在第 4 天（股票价格 = 3）的时候买入，在第 5 天（股票价格 = 6）的时候卖出, 这笔交易所能获得利润 = 6-3 = 3 。
+ */
+public class MaxProfit {
+    public int maxProfit(int[] prices) {
+        boolean canBuy = true;
+        int maxPro = 0;
+        int buyPrice = 0;
+        for (int i = 0; i < prices.length - 1; i++) {
+            if (prices[i] < prices[i + 1] && canBuy) {
+                buyPrice = prices[i];
+                canBuy = false;
+            }
+            if (prices[i] > prices[i + 1] && !canBuy) {
+                maxPro += (prices[i] - buyPrice);
+                canBuy = true;
+            }
+            if (!canBuy && i == prices.length - 2) {
+                maxPro += (prices[prices.length - 1] - buyPrice);
+            }
+        }
+        return maxPro;
+    }
+}
