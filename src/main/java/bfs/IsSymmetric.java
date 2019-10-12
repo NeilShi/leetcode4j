38733875e953d1dcc9ddd0c2a5881f01e56297a4
neilshi <627911861@qq.com>
2019-10-12 17:50:12 +0800

    add IsSymmetric

---
+++ src/main/java/bfs/IsSymmetric.java
@@ -0,0 +1,48 @@
+package bfs;
+
+import datastructure.TreeNode;
+import java.util.LinkedList;
+
+/**
+ * 给定一个二叉树，检查它是否是镜像对称的。
+ *
+ * 例如，二叉树 [1,2,2,3,4,4,3] 是对称的。
+ *
+ *     1
+ *    / \
+ *   2   2
+ *  / \ / \
+ * 3  4 4  3
+ * 但是下面这个 [1,2,2,null,3,null,3] 则不是镜像对称的:
+ *
+ *    1
+ *   / \
+ *  2   2
+ *  \   \
+ *  3    3
+ */
+public class IsSymmetric {
+    /**
+     * todo
+     * */
+    public boolean isSymmetric(TreeNode root) {
+        if (root == null) {
+            return true;
+        }
+        LinkedList<TreeNode> queue = new LinkedList<>();
+        queue.add(root);
+        queue.add(root);
+        while (!queue.isEmpty()) {
+            TreeNode node1 = queue.remove();
+            TreeNode node2 = queue.remove();
+            if (node1 == null && node2 == null) return true;
+            if (node1 == null || node2 == null) return false;
+            if (node1.val != node2.val) return false;
+            queue.add(node1.left);
+            queue.add(node2.right);
+            queue.add(node1.right);
+            queue.add(node2.left);
+        }
+        return true;
+    }
+}
