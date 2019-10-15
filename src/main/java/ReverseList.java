800df352fb1d1496a79f9e7ac66c21afb446109d
neilshi <627911861@qq.com>
2019-10-16 00:40:52 +0800

    add ReverseList

---
+++ src/main/java/ReverseList.java
@@ -0,0 +1,26 @@
+import datastructure.ListNode;
+
+/**
+ * 反转一个单链表。
+ * <p>
+ * 示例:
+ * <p>
+ * 输入: 1->2->3->4->5->NULL
+ * 输出: 5->4->3->2->1->NULL
+ * <p>
+ * 进阶:
+ * 你可以迭代或递归地反转链表。你能否用两种方法解决这道题？
+ */
+public class ReverseList {
+    public ListNode reverseList(ListNode head) {
+        ListNode prev = null;
+        ListNode curr = head;
+        while (curr != null) {
+            ListNode temp = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr = temp;
+        }
+        return prev;
+    }
+}
