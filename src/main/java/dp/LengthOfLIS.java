e7a55fa37f0e934f30a45020154d7914d080ed60
neilshi <627911861@qq.com>
2019-10-19 02:02:24 +0800

    add annotation

--- src/main/java/dp/LengthOfLIS.java
+++ src/main/java/dp/LengthOfLIS.java
@@ -24,8 +24,8 @@ public class LengthOfLIS {
     public int lengthOfLIS(int[] nums) {
         if (nums.length == 0) return 0;
         int[] dp = new int[nums.length];
+        // 此处的stream操作改用for循环，能节省2/3左右的时间
         dp = Arrays.stream(dp).map(x -> 1).toArray();
-        dp[0] = 1;
         int maxVal = 1;
         for (int i = 1; i < nums.length; i++) {
             for (int j = 0; j < i; j++) {
