d288e77cf3c6fc8629ac9c9452b8f0264d83b99b
neilshi <627911861@qq.com>
2019-10-22 09:29:37 +0800

    review BinarySearch

--- src/main/java/binarySearch/BinarySearch.java
+++ src/main/java/binarySearch/BinarySearch.java
@@ -25,17 +25,14 @@ public class BinarySearch {
     public int search(int[] nums, int target) {
         int left = 0;
         int right = nums.length - 1;
-        int mid = 0;
         while (left < right) {
-            mid = (left + right + 1) >>> 1;
+            int mid = (left + right + 1) >>> 1;
             if (nums[mid] > target) {
                 right = mid - 1;
             } else {
                 left = mid;
             }
         }
-        if (nums[mid] == target) return mid;
-        if (mid > 0 && nums[mid - 1] == target) return mid - 1;
-        return -1;
+        return nums[left] == target ? left : -1;
     }
 }
