c91bf13e96aa2f868177d6299e502254f76317c8
Neil Shi <neil.shi@oocl.com>
2019-10-29 18:05:49 +0800

    ac MyStack

---
+++ src/main/java/MyStack.java
@@ -0,0 +1,53 @@
+/**
+ * Your MyStack object will be instantiated and called as such:
+ * MyStack obj = new MyStack();
+ * obj.push(x);
+ * int param_2 = obj.pop();
+ * int param_3 = obj.top();
+ * boolean param_4 = obj.empty();
+ */
+
+
+import java.util.LinkedList;
+
+/**
+ * Initialize your data structure here.
+ */
+class MyStack {
+
+    /**
+     * Initialize your data structure here.
+     */
+    private LinkedList<Integer> queue = new LinkedList<>();
+    public MyStack() {
+
+    }
+
+    /**
+     * Push element x onto stack.
+     */
+    public void push(int x) {
+        queue.addFirst(x);
+    }
+
+    /**
+     * Removes the element on top of the stack and returns that element.
+     */
+    public int pop() {
+        return queue.removeFirst();
+    }
+
+    /**
+     * Get the top element.
+     */
+    public int top() {
+        return queue.getFirst();
+    }
+
+    /**
+     * Returns whether the stack is empty.
+     */
+    public boolean empty() {
+        return queue.isEmpty();
+    }
+}
